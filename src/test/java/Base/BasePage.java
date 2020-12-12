0f9bc89bd7388c152b85f6e4ac587d4fa8042355
Kaan <kaankarabulutt1@gmail.com>
2020-12-12 23:09:43 +0300

    feat: added title, year, released controls

--- src/test/java/Base/BasePage.java
+++ src/test/java/Base/BasePage.java
@@ -4,16 +4,18 @@ import io.restassured.RestAssured;
 import org.junit.After;
 import org.junit.Before;
 
+import static io.restassured.RestAssured.baseURI;
+
 public class BasePage {
 
     @Before
-    public void setUp(){
-        RestAssured.baseURI="http://www.omdbapi.com";
+    public void setUp() {
+        RestAssured.baseURI = "http://www.omdbapi.com";
     }
 
     @After
-    public void AfterClass(){
-        System.out.println("İşlem Başarılı");
+    public void AfterClass() {
+        System.out.println("Process completed successfully.");
     }
 
 }
