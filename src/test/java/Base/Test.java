7d7ae8305076a15562ec6f98916169cd192a9985
Kaan <kaankarabulutt1@gmail.com>
2020-12-23 17:14:40 +0300

    feat: assertionError added

--- src/test/java/Base/Test.java
+++ src/test/java/Base/Test.java
@@ -2,13 +2,14 @@ package Base;
 
 import io.restassured.response.Response;
 import jdk.jfr.Description;
-
+import static org.junit.Assert.assertNotEquals;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.emptyOrNullString;
 import static org.hamcrest.Matchers.not;
 import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertNotEquals;
 
 public class Test extends BasePage {
     public static List<String> BySearch() {
@@ -28,7 +29,6 @@ public class Test extends BasePage {
 
             while (size < titleSize) {
                 title = response.jsonPath().getString("Search.Title[" + size + "]");
-
                 if (title.contains("Harry Potter and the Sorcerer's Stone")) {
 
                     imdbId = response.jsonPath().getString("Search.imdbID[" + size + "]");
@@ -73,5 +73,13 @@ public class Test extends BasePage {
 
         System.out.println("---By Id Or Title---");
         ByIdorTitle(List.get(1));
+    }
+
+    @org.junit.Test
+    @Description("Testing imdbId is Null")
+    public void listIsNullScenario() {
+        List<String> List = BySearch();
+
+        assertNotEquals(0, List.size());
     }
 }
